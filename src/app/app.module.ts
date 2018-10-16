1de4deb417df1cb8df0254b57fdc7cdff34cdd40
mfourzans <mfourzans@sistemasesco.com>
2018-10-16 15:56:43 -0300

    Adding new components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
-import { AddCardComponent } from './add-card/add-card.component';
-import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { AddComponent } from './pages/add/add.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
 @NgModule({
@@ -16,7 +19,10 @@ import { DetailsComponent } from './details/details.component';
     HomeComponent,
     WeatherCardComponent,
     AddCardComponent,
-    DetailsComponent
+    DetailsComponent,
+    AddComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
